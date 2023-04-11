410787a2843c4a1ce1ad632b0d57fb79358ddb36
Jasti Sri Radhe Shyam <radheshyam.jasti@zeeve.io>
2023-04-11 19:36:01 +0530

    fix dashboard lint
    Signed-off-by: Jasti Sri Radhe Shyam <radheshyam.jasti@zeeve.io>

--- packages/ui/src/pages/dashboard/page.tsx
+++ packages/ui/src/pages/dashboard/page.tsx
@@ -1,19 +1,35 @@
-import iconNetwork from "./assets/My-Network.svg";
-import iconTemplate from "./assets/template.svg";
-import iconActivity from "./assets/Activity.svg";
+import iconNetwork from '../../components/assets/My-Network.svg';
+import iconTemplate from '../../components/assets/template.svg';
+import iconActivity from '../../components/assets/Activity.svg';
 import Card from './components/card';
 
 export default function Dashboard() {
   return (
     <div className='h-full bg-black p-6'>
-      <div className="bg-create-button flex flex-col gap-6 p-6 border-2 border-border  rounded-2xl  border-solid box-border max-w-max	">
-        <h3 className='text-white font-rubik text-xl	font-bold'>Explore Dashboard</h3>
+      {/* eslint-disable-next-line max-len */}
+      <div className='bg-create-button flex flex-col gap-6 p-6 border-2 border-border rounded-2xl border-solid box-border max-w-max'>
+        <h3 className='text-white font-rubik text-xl font-bold'>Explore Dashboard</h3>
         <div className='flex flex-row gap-6 flex-wrap'>
-          <Card cardTitle='Networks' cardLink={'/network'} cardIconSrc={iconNetwork} cardDescription={'Create and list networks'}/>
-          <Card cardTitle='Templates' cardLink={'/template'} cardIconSrc={iconTemplate} cardDescription={'Create and list templates'}/>
-          <Card cardTitle='Activity' cardLink={'/activity'} cardIconSrc={iconActivity} cardDescription={'List activity actions'}/>
+          <Card
+            cardTitle='Networks'
+            cardLink='/network'
+            cardIconSrc={iconNetwork}
+            cardDescription='Create and list networks'
+          />
+          <Card
+            cardTitle='Templates'
+            cardLink='/template'
+            cardIconSrc={iconTemplate}
+            cardDescription='Create and list templates'
+          />
+          <Card
+            cardTitle='Activity'
+            cardLink='/activity'
+            cardIconSrc={iconActivity}
+            cardDescription='List activity actions'
+          />
         </div>
       </div>
-    </div >
+    </div>
   );
 }

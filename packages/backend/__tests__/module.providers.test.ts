2e9b7f49b5981a565ebbebc6d360fbbeda55c1a4
Antar Basu <antar.basu@zeeve.io>
2023-04-18 11:23:16 +0530

    update test cases

--- packages/backend/__tests__/module.providers.test.ts
+++ packages/backend/__tests__/module.providers.test.ts
@@ -1,31 +1,29 @@
-// import { test, expect, describe } from 'vitest'
-// import { networkCleanUp } from '../src/modules/providers/common';
-// import { getNamespace, cleanUp, deleteDirUnshare } from '../src/modules/providers/podman';
+import { test, describe } from 'vitest'
+import { networkCleanUp } from '../src/modules/providers/common';
+import { getNamespace, cleanUp, deleteDirUnshare } from '../src/modules/providers/podman';
 
 
-// describe('networkCleanUp', () => {
-//     test("networkCleanUp", async () => {
-//         const result = networkCleanUp('podman','TestNetwork','/home/antar/Desktop/Example');
-//         // console.log(result);
-//     })
-// })
+describe('networkCleanUp', () => {
+    test("networkCleanUp", async () => {
+        const result = networkCleanUp('podman','TestNetwork','/home/antar/Desktop/Example');
+    })
+})
 
-// describe('getNamespace', () => {
-//     test("getNamespace", async () => {
-//         const result = getNamespace('Example');
-//         // console.log(result);
-//     })
-// })
+describe('getNamespace', () => {
+    test("getNamespace", async () => {
+        const result = getNamespace('Example');
+    })
+})
 
-// describe('cleanUp', () => {
-//     test("cleanUp", async () => {
-//         const result = cleanUp('','');
-//     })
-// })
-// describe('deleteDirUnshare', () => {
-//     test("deleteDirUnshare", async () => {
-//         const result = deleteDirUnshare('','');
-//     })
-// })
+describe('cleanUp', () => {
+    test("cleanUp", async () => {
+        const result = cleanUp('','');
+    })
+})
+describe('deleteDirUnshare', () => {
+    test("deleteDirUnshare", async () => {
+        const result = deleteDirUnshare('','');
+    })
+})
 
 

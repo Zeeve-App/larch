7b74559f670587d29a2198306ded7ba7256769ce
abhishek <abhishek.kumar@zeeve.io>
2023-04-20 10:49:26 +0530

    Static filters html removed

--- packages/ui/src/pages/template/page.tsx
+++ packages/ui/src/pages/template/page.tsx
@@ -6,15 +6,6 @@ export default function Template() {
   return (
     <div className='p-6 gap-6 flex-col flex'>
       <div className='h-12 w-full flex'>
-        <div className='h-12 bg-black border-2 border-border rounded flex'>
-          <form className='flex w-full justify-between px-4'>
-            <input
-              className='form-control text-white focus:outline-none w-full bg-black me-2 font-rubik text-base'
-              placeholder='Search...'
-            />
-            <img className='w-6 h-6 m-2.5' src={IconImage} alt='' />
-          </form>
-        </div>
         <div className='h-full flex w-full flex-wrap content-center item-center justify-end gap-4'>
           <div className='item-center'>
             {/* eslint-disable-next-line max-len */}
@@ -23,18 +14,6 @@ export default function Template() {
                 <span>Create Network</span>
               </Link>
             </button>
-          </div>
-          {/* eslint-disable-next-line max-len */}
-          <div className='bg-create-button gap-2 text-white font-rubik flex flex-row border-2 border-border rounded h-10  items-center'>
-            <div className='border-r-2 flex flex-row items-start py-1.5 px-4 border-border h-full'>
-              <select className='bg-create-button'>
-                <option className='text-black bg-white'>Filter By</option>
-                <option className='text-black bg-white'>option 2</option>
-                <option className='text-black bg-white'>option 3</option>
-                <option className='text-black bg-white'> option 4 </option>
-                <option className='text-black bg-white'> option 5 </option>
-              </select>
-            </div>
           </div>
         </div>
       </div>

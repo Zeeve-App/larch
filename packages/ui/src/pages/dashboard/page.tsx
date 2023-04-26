ed81c955bed09b94c06be07efcf4e748727b00d4
Abhishek Kumar <abhishek.kumar@zeeve.io>
2023-04-26 11:36:37 +0530

    API call in setInterval

--- packages/ui/src/pages/dashboard/page.tsx
+++ packages/ui/src/pages/dashboard/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { useEffect, useState } from 'react';
 import iconNetwork from '../../components/assets/My-Network.svg';
 import iconTemplate from '../../components/assets/template.svg';
@@ -5,8 +6,13 @@ import iconActivity from '../../components/assets/Activity.svg';
 import IconEdit from '../../components/assets/editor.svg';
 import Card from './components/card';
 import {
-  getLarchVersionInfo, getNetworkList, getRunList, getTemplateList, getUserActivityList,
+  getLarchVersionInfo,
+  getNetworkList,
+  getRunList,
+  getTemplateList,
+  getUserActivityList,
 } from '../../utils/api';
+import Loader from '../../components/loader';
 
 export default function Dashboard() {
   const [networkRecords, setNetworkRecords] = useState('NA');
@@ -15,32 +21,58 @@ export default function Dashboard() {
   const [activityRecords, setActivityRecords] = useState('NA');
   const [supportedZombienetVersion, setSupportedZombienetVersion] = useState('');
   const [larchVersion, setLarchVersion] = useState('');
+  const [isShowLoader, setIsShowLoader] = useState<boolean>(true);
 
   useEffect(() => {
-    const networkRecordsRes = getNetworkList({ meta: { numOfRec: 0 } }).then((data) => data.meta.total);
-    const templateRecordsRes = getTemplateList({ meta: { numOfRec: 0 } }).then((data) => data.meta.total);
-    const runListRecordsRes = getRunList({ meta: { numOfRec: 0 } }).then((data) => data.meta.total);
-    const activityRecordsRes = getUserActivityList({ meta: { numOfRec: 0 } }).then((data) => data.meta.total);
-    Promise.allSettled([networkRecordsRes, templateRecordsRes, runListRecordsRes, activityRecordsRes])
-      .then((values) => {
-        setNetworkRecords(values[0].status === 'fulfilled' ? (values[0].value).toString(10) : 'NA');
-        setTemplateRecords(values[1].status === 'fulfilled' ? (values[1].value).toString(10) : 'NA');
-        setRunListRecords(values[2].status === 'fulfilled' ? (values[2].value).toString(10) : 'NA');
-        setActivityRecords(values[3].status === 'fulfilled' ? (values[3].value).toString(10) : 'NA');
-      });
-    getLarchVersionInfo().then(({ result }) => {
-      setLarchVersion(result.larchVersion);
-      setSupportedZombienetVersion(result.zombienetVersion);
-    }).catch(() => {
-      setLarchVersion('NA');
-      setSupportedZombienetVersion('NA');
+    const networkRecordsRes = getNetworkList({ meta: { numOfRec: 0 } }).then(
+      (data) => data.meta.total,
+    );
+    const templateRecordsRes = getTemplateList({ meta: { numOfRec: 0 } }).then(
+      (data) => data.meta.total,
+    );
+    const runListRecordsRes = getRunList({ meta: { numOfRec: 0 } }).then(
+      (data) => data.meta.total,
+    );
+    const activityRecordsRes = getUserActivityList({
+      meta: { numOfRec: 0 },
+    }).then((data) => data.meta.total);
+    Promise.allSettled([
+      networkRecordsRes,
+      templateRecordsRes,
+      runListRecordsRes,
+      activityRecordsRes,
+    ]).then((values) => {
+      setNetworkRecords(
+        values[0].status === 'fulfilled' ? values[0].value.toString(10) : 'NA',
+      );
+      setTemplateRecords(
+        values[1].status === 'fulfilled' ? values[1].value.toString(10) : 'NA',
+      );
+      setRunListRecords(
+        values[2].status === 'fulfilled' ? values[2].value.toString(10) : 'NA',
+      );
+      setActivityRecords(
+        values[3].status === 'fulfilled' ? values[3].value.toString(10) : 'NA',
+      );
     });
+    getLarchVersionInfo()
+      .then(({ result }) => {
+        setLarchVersion(result.larchVersion);
+        setSupportedZombienetVersion(result.zombienetVersion);
+        // setIsShowLoader(false);
+      })
+      .catch(() => {
+        setLarchVersion('NA');
+        setSupportedZombienetVersion('NA');
+      });
   }, []);
   return (
-    <div className='h-full w-full bg-black p-6'>
-      {/* eslint-disable-next-line max-len */}
+    <div className='h-full w-full bg-black p-6 relative'>
+      {isShowLoader && <Loader />}
       <div className='bg-create-button flex flex-col gap-6 p-6 border-2 border-border rounded-2xl border-solid box-border'>
-        <h3 className='text-white font-rubik text-2xl font-bold text-center'>Explore Larch</h3>
+        <h3 className='text-white font-rubik text-2xl font-bold text-center'>
+          Explore Larch
+        </h3>
         <div className='text-white font-rubik w-1/2 font-bold  border-solid border-dark-green border-4 rounded-xl p-2'>
           <div className='flex flex-row'>
             <div className='w-10/12'>Supported Zombienet version</div>

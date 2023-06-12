c6bea5de1c2b33ecc84971e0d1ae27a35f39a2fa
Jasti Sri Radhe Shyam <radheshyam.jasti@zeeve.io>
2023-06-12 17:51:49 +0530

    update supported zombienet version
    Signed-off-by: Jasti Sri Radhe Shyam <radheshyam.jasti@zeeve.io>

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -19,7 +19,7 @@ import * as versionInfo from './version.js';
 export const LARCH_CONTEXT_DIR = `${homedir()}/.larch`;
 export const LARCH_VERSION = versionInfo.default.version;
 export const LARCH_DEFAULT_PROVIDER_NAME = 'podman';
-export const ZOMBIENET_VERSION: string = '1.3.43';
+export const ZOMBIENET_VERSION: string = '1.3.55';
 export const ZOMBIENET_BIN_COLLECTION_DIR = `${LARCH_CONTEXT_DIR}/bin`;
 export const ZOMBIENET_NETWORKS_COLLECTION_DIR = `${LARCH_CONTEXT_DIR}/networks`;
 export const ZOMBIENET_NETWORKS_EXECUTION_DIR = `${LARCH_CONTEXT_DIR}/exec`;

e959ce1e3edb76e223f68b8e5ebb02c40b06ce26
Seraya Takahashi <seraya@web3.foundation>
2023-07-13 23:07:25 +0200

    improve docs in ui

--- packages/ui/src/pages/documentation/page.tsx
+++ packages/ui/src/pages/documentation/page.tsx
@@ -75,13 +75,13 @@ export default function Documentation() {
             About
           </h2>
           <ul className="list-circle grid gap-3 pl-3 text-[18px]">
-            <li>Larch is a UI component for Zombienet CLI.</li>
+            <li>Larch is a UI component for the <a href="https://github.com/paritytech/zombienet" target="_blank">Zombienet CLI.</a></li>
             <li>
-              With Larch as a Zombienet's UI, one can create network and test
-              configuration with assistance and ease.
+              With Larch Zombienet's UI, users can create network and test
+              configurations with assistance and ease.
             </li>
             <li>
-              Larch also provide Network management and logs view capabilities.
+              Larch also provides network management and logs view capabilities.
             </li>
           </ul>
         </section>
@@ -95,7 +95,7 @@ export default function Documentation() {
               <div className="grid gap-1">
                 <h3 id="tutorial" className="text-[24px]">Tutorial</h3>
                 <p className="text-[18px] leading-8">
-                  The Following link contains the Larch tutorial : <a className="text-blue-600 visited:text-purple-600" href="https://github.com/Zeeve-App/larch/blob/main/docs/user/tutorial.md" target="_blank" rel="noopener noreferrer">Larch Tutorial</a>
+                  The following link contains the Larch tutorial : <a className="text-blue-600 visited:text-purple-600" href="https://github.com/Zeeve-App/larch/blob/main/docs/user/tutorial.md" target="_blank" rel="noopener noreferrer">Larch Tutorial</a>
                 </p>
               </div>
             </div>
@@ -103,7 +103,7 @@ export default function Documentation() {
               <div className="grid gap-1">
                 <h3 id="create-template" className="text-[24px]">Create Template</h3>
                 <p className="text-[18px] leading-8">
-                  Larch manages Zombienet based network configuration as
+                  Larch manages Zombienet-based network configurations as
                   templates, which can be used to deploy the networks.
                 </p>
               </div>
@@ -119,32 +119,31 @@ export default function Documentation() {
                   <li>Test Configuration</li>
                 </ul>
                 <p className="text-[18px] leading-8">
-                  The combination of these configuration is used to create
-                  Zombienet Network & Testing configuration.
+                  The combination of these configurations is used to create
+                  Zombienet networks & test configurations.
                 </p>
               </div>
             </div>
             <div className="grid gap-6 p-6 rounded-xl border-4 border-larch-dark_3">
               <div className="grid gap-1">
                 <h3 id="create-network" className="text-[24px]">
-                  Create Network (Evaluation & Testing)
+                  Create a Network (Evaluation & Testing)
                 </h3>
               </div>
               <div className="grid gap-3">
                 <p className="text-[18px] leading-8">
-                  Network's can be created for both Evaluation & Testing.
+                  Networks can be created for both Evaluation & Testing.
                 </p>
                 <ul className="list-circle grid gap-3 pl-6 text-[18px]">
                   <li>
-                    Evaluation network is a network used for do manual
-                    transactions and additional evaluations of the network as
+                    An evaluation network is a network used for dispatching manual
+                    transactions and conducting additional evaluations of the network as
                     required.
                   </li>
                   <li>
-                    Testing network is a network which will create network based
-                    on network configuration and then do specified operations in
-                    testing configuration and then deletes the network once the
-                    network is tested.
+                    A testing network is a network that's based on a network configuration
+                    and does specified operations according to the testing configuration
+                    and finally deletes the network once it's is tested.
                   </li>
                 </ul>
               </div>
@@ -153,8 +152,8 @@ export default function Documentation() {
               <div className="grid gap-1">
                 <h3 id="network-management" className="text-[24px]">Network Management</h3>
                 <p className="text-[18px] leading-8">
-                  Once a network is created for either evaluating or testing the network, a certain
-                  operations can be performed on the network and these are:
+                  Once a network is created for the purpose of either evaluating or testing it,
+                  certain operations can be performed on the network:
                   <ul className="list-circle grid gap-3 pl-6 text-[18px]">
                     <li>Delete Network</li>
                     <li>View Command</li>
@@ -163,21 +162,21 @@ export default function Documentation() {
                 </p>
                 <h4 id="network-management" className="text-[18px]">Delete Network</h4>
                 <p className="text-[18px] leading-8">
-                  Deletion of network can be done at any stage of network lifecycle (creation, running, etc.).<br />
-                  Deletion can be performed by navigating to My Network page and then click on delete icon under Action column present against the network record,
-                  then confirmation dialog box will appear, click on confirm to initiate deletion process.
+                  Deletion of a network can be done at any stage of its lifecycle (creation, running, etc.).<br />
+                  Deletion can be performed by navigating to the <i>My Network</i> page, clicking on the delete icon under <i>Action</i> column present against the network record and finally
+                  clicking on <i>confirm</i> in the confirmation dialog to initiate the deletion process.
                 </p>
                 <h4 id="network-management" className="text-[18px]">View Command</h4>
                 <p className="text-[18px] leading-8">
-                  Zombienet command can be viewed for which it was issued to create the network<br />
-                  To view the command navigate to network page, and click on eye icon under action column present against the network record, this will navigate to executions page with network filtered.<br />
-                  In the execution record under Operation column click on the command button, this will open command details.
+                  Zombienet command can be viewed for which it was issued to create the network.<br />
+                  To view the command navigate to the network page, and click on the eye icon in the <i>action</i> column present against the network record - this will navigate to the <i>executions</i> page with the network filtered.<br />
+                  In the execution record in the <i>Operation</i> column click on the command button, this will open the command details.
                 </p>
                 <h4 id="network-management" className="text-[18px]">View Output</h4>
                 <p className="text-[18px] leading-8">
-                  Zombienet CLI output can be viewed for a created the network<br />
-                  To view the output, navigate to network page, and click on eye icon under action column present against the network record, this will navigate to executions page with the network filtered.<br />
-                  In the execution record under Operation column click on the output button, this will open output for the Zombienet. The Output window contains both Standard output and Standard Error streams.
+                  The Zombienet CLI output can be viewed for a created the network<br />
+                  To view the output, navigate to network page, and click on eye icon under action column present against the network record, this will navigate to the <i>executions</i> page with the network filtered.<br />
+                  In the execution record in the <i>Operation</i> column click on the output button, this will open the output for the Zombienet. The Output window contains both Standard output and Standard Error streams.
                 </p>
               </div>
             </div>
@@ -192,7 +191,7 @@ export default function Documentation() {
               <div className="grid gap-1">
                 <h3 id="dashboard" className="text-[24px]">Dashboard</h3>
                 <p className="text-[18px] leading-8">
-                  Dashboard contains the stats and version info of the Larch
+                  The Dashboard contains the stats and version info of the Larch
                   application.
                 </p>
               </div>
@@ -201,15 +200,15 @@ export default function Documentation() {
               <div className="grid gap-1">
                 <h3 id="networks" className="text-[24px]">Networks</h3>
                 <p className="text-[18px] leading-8">
-                  Network page contains the listing of created networks for
+                  The Network page contains the listing of created networks for
                   either Evaluation or Testing.
                 </p>
               </div>
               <div className="grid gap-3">
                 <p className="text-[18px] leading-8">Features</p>
                 <ul className="list-circle grid gap-3 pl-6 text-[18px]">
-                  <li>Listing of Networks</li>
-                  <li>Delete network, View Logs & Invocation Command</li>
+                  <li>List networks</li>
+                  <li>Delete networks, view logs & invoke commands</li>
                 </ul>
               </div>
             </div>
@@ -217,16 +216,16 @@ export default function Documentation() {
               <div className="grid gap-1">
                 <h3 id="template" className="text-[24px]">Template</h3>
                 <p className="text-[18px] leading-8">
-                  List Network templates and manage them.
+                  List network templates and manage them.
                 </p>
               </div>
               <div className="grid gap-3">
                 <p className="text-[18px] leading-8">Features</p>
                 <ul className="list-circle grid gap-3 pl-6 text-[18px]">
-                  <li>Listing of Network Templates</li>
+                  <li>List Network Templates</li>
                   <li>
-                    Edit, Delete, Create Network (Evaluation), Create Test
-                    Network (For Test Network), Duplicate Template Network
+                    Edit, Delete, Create Networks (Evaluations), Create Test
+                    Networks (to test networks), Duplicate Template Network
                   </li>
                 </ul>
               </div>
